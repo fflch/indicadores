98564ca2d0b8bdb0f23f010ce3d91380b66df753
Anna Valim <annacarolinavalim@gmail.com>
2025-02-04 14:32:59 -0300

    alterações na tabela

--- src/Controller/IndicadoresController.php
+++ src/Controller/IndicadoresController.php
@@ -17,16 +17,22 @@ class IndicadoresController extends ControllerBase {
 
     $submission = \Drupal\webform\Entity\WebformSubmission::load($submission_id);
     $data = $submission->getData();
+    
+    $unidade = $submission->getOwner()->getDisplayName();
 
     $linhas = '';
     foreach($data['formulario_coleta_de_dados'] as $linha){
       $linhas .= "    
       <tr>
+        <td>{$unidade}</td>
         <td>{$linha['tipo']}</td>
         <td>{$linha['nome']}</td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td>{$linha['publico']}</td>
+        <td>{$linha['carga_horaria']}</td>
+        <td>{$linha['participantes']}</td>
+        <td>{$linha['data_inicio']}</td>
+        <td>{$linha['data_termino']}</td>
+        <td>{$linha['ministrante']}</td>
       </tr>";
     }
 
@@ -34,11 +40,15 @@ class IndicadoresController extends ControllerBase {
     <table>
       <thead>
         <tr>
-          <th>Tipo</th>
+          <th>Unidade</th>
+          <th>Tipo de Capacitação</th>
           <th>Nome</th>
-          <th></th>
-          <th></th>
-          <th></th>
+          <th>Público</th>
+          <th>Carga Horária</th>
+          <th>Nº de Participantes/Alunos Matriculados</th>
+          <th>Data de Início</th>
+          <th>Data de Término</th>
+          <th>Ministrante</th>
         </tr>
       </thead>
       <tbody>
